0d557893efee027468b782cddd59e7b32f9b902a
viqueen <hasnae@viqueen.org>
2025-01-27 13:46:27 +1100

    welcome load env helper

---
+++ src/cli/load-env.ts
@@ -0,0 +1,44 @@
+/**
+ * Copyright <update-me> <update-me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import dotenv from 'dotenv';
+
+interface Environment {
+    CONFLUENCE_SITE_NAME: string;
+    CONFLUENCE_USERNAME: string;
+    CONFLUENCE_API_TOKEN: string;
+    TARGET_SITE: string;
+    GOOGLE_ANALYTICS_TRACKING_ID?: string;
+}
+
+const loadEnv = (): Environment => {
+    const parsed = (dotenv.config().parsed || {}) as unknown as Environment;
+    return {
+        CONFLUENCE_SITE_NAME:
+            parsed.CONFLUENCE_SITE_NAME ?? process.env.CONFLUENCE_SITE_NAME,
+        CONFLUENCE_USERNAME:
+            parsed.CONFLUENCE_USERNAME ?? process.env.CONFLUENCE_USERNAME,
+        CONFLUENCE_API_TOKEN:
+            parsed.CONFLUENCE_API_TOKEN ?? process.env.CONFLUENCE_API_TOKEN,
+        TARGET_SITE: parsed.TARGET_SITE ?? process.env.TARGET_SITE,
+        GOOGLE_ANALYTICS_TRACKING_ID:
+            parsed.GOOGLE_ANALYTICS_TRACKING_ID ??
+            process.env.GOOGLE_ANALYTICS_TRACKING_ID
+    };
+};
+
+export { loadEnv };
+export type { Environment };
